306b40285c62099bb2990661f225453a69a4d2c4
Moorgal <megaboki@gmail.com>
2023-01-21 01:02:16 +0000

    variables, randomIndex, pseudocode

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,52 @@
-console.log('hello logic.js');
+// variables
+const time = document.querySelector('#time');
+const startButton = document.querySelector('#start');
+const questions = document.querySelector('#questions');
+const questionTitle = document.querySelector('#question-title');
+const questionChoices = document.querySelector('#choices');
+const endScreen = document.querySelector('#end-screen');
+const finalScore = document.querySelector('#final-score');
+const initials = document.querySelector('#initials');
+const feedback = document.querySelector('#feedback');
+console.log(feedback);
+
+// function to create a random index
+function randomIndex(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  let index = arr[randomIndex];
+  return index;
+}
+
+// set up timer
+
+// when start pressed <div id="questions" class="hide"> switch on
+// -----------------questions.classList.remove('hide');---------------
+
+// question-title
+// questionTitleText = questions[0].question;
+// console.log(questionTitleText);
+// innercontent
+// choices
+// questionChoicesText = questions[0].answers[2];
+// console.log(questionChoicesText);
+// innerhtml for loop button
+// randomized
+
+// if correct
+
+// if not correct
+// if not correct subtract from clock
+
+// end of the game <div id="end-screen" class="hide"> switch on
+// ----------------endScreen.classList.remove('hide');---------------
+
+// <p>Your final score is <span id="final-score"></span>.</p>
+// show final points
+
+// submit initials
+
+// submit initials to highscores
+
+// feedback respond on correct or not correct
+
+// delete highscores
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,31 +1,31 @@
 console.log('hello questions.js');
 // I got the quiz questions and answers from w3school.com js quiz
 
-const question = [
-  { question0: 'Inside which HTML element do we put the JavaScript?', answers: ['<javascript>', '<js>', '<script>', '<scripting>'], correctAnswer: '<script>' },
+const questions = [
+  { question: 'Inside which HTML element do we put the JavaScript?', answers: ['<javascript>', '<js>', '<script>', '<scripting>'], correctAnswer: '<script>' },
   {
-    question1: 'Where is the correct place to insert a JavaScript?',
+    question: 'Where is the correct place to insert a JavaScript?',
     answers: ['The <body> section', 'The <head> section', 'The <header> section', 'The <footer> section'],
     correctAnswer: 'The <body> section',
   },
   {
-    question2: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-    answers: ['<script src=xxx.js>', '<script href=xxx.js>', '<script name=xxx.js>', answer4],
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    answers: ['<script src=xxx.js>', '<script href=xxx.js>', '<script name=xxx.js>'],
     correctAnswer: '<script src=xxx.js>',
   },
-  { question3: 'The external JavaScript file must contain the <script> tag.', answers: ['false', 'true', answer3, answer4], correctAnswer: 'false' },
+  { question: 'The external JavaScript file must contain the <script> tag.', answers: ['false', 'true'], correctAnswer: 'false' },
   {
-    question4: 'How do you write "Hello World" in an alert box?',
+    question: 'How do you write "Hello World" in an alert box?',
     answers: ['alertBox("Hello World")', 'msgBox("Hello World")', 'alert("Hello World")', 'msg("Hello World")'],
     correctAnswer: 'alert("Hello World")',
   },
   {
-    question5: 'How do you create a function in JavaScript?',
-    answers: ['function = myFunction()', 'function:myFunction()', 'function myFunction()', answer4],
+    question: 'How do you create a function in JavaScript?',
+    answers: ['function = myFunction()', 'function:myFunction()', 'function myFunction()'],
     correctAnswer: 'function myFunction()',
   },
-  { question6: 'How do you call a function named "myFunction"?', answers: ['myFunction()', 'call myFunction()', 'call function myFunction()', answer4], correctAnswer: 'myFunction()' },
-  { question7: 'How to write an IF statement in JavaScript?', answers: ['if i == 5 then', 'if i = 5 then', 'if i = 5', 'if (i == 5)'], correctAnswer: 'if (i == 5)' },
-  { question8: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?', answers: ['if i <> 5', 'if (i != 5)', 'if i =! 5 then', 'if (i<>5)'], correctAnswer: 'if (i != 5)' },
-  { question9: 'Which event occurs when the user clicks on an HTML element?', answers: ['onmouseover', 'onclick', 'onchange', 'onmouseclick'], correctAnswer: 'onclick' },
+  { question: 'How do you call a function named "myFunction"?', answers: ['myFunction()', 'call myFunction()', 'call function myFunction()'], correctAnswer: 'myFunction()' },
+  { question: 'How to write an IF statement in JavaScript?', answers: ['if i == 5 then', 'if i = 5 then', 'if i = 5', 'if (i == 5)'], correctAnswer: 'if (i == 5)' },
+  { question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?', answers: ['if i <> 5', 'if (i != 5)', 'if i =! 5 then', 'if (i<>5)'], correctAnswer: 'if (i != 5)' },
+  { question: 'Which event occurs when the user clicks on an HTML element?', answers: ['onmouseover', 'onclick', 'onchange', 'onmouseclick'], correctAnswer: 'onclick' },
 ];
